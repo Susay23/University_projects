5cab187b0728a7532016cb36d8bbffcf74bf7bef
Susay23 <buituananh29112006@gmail.com>
2026-06-09 01:11:10 +0000

    Implement initial graph representation and build system setup
    - Added main.cpp to read graph data from a file and represent it as an adjacency matrix.
    - Created CMake build system files for project configuration and management.
    - Introduced run.sh script for building and executing the project.
    - Removed obsolete question1 file.
    - Added necessary CMake files for dependency management and build progress tracking.

---
+++ Graph Theory/session/session 1/build/CMakeFiles/session1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session1.dir/link.d"
+  "CMakeFiles/session1.dir/main.cpp.o"
+  "CMakeFiles/session1.dir/main.cpp.o.d"
+  "session1"
+  "session1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Graph Theory/session/session 1/main.cpp
+++ Graph Theory/session/session 1/main.cpp
@@ -6,21 +6,25 @@ int main(){
     int m,n;
     fscanf(f, "%d%d", &m, &n);
 
-    int graph[m][n];
-    for(int i=0; i<m;i++){
-        for(int j=0; j<n;j++){
+    int graph[m+1][n+1];
+    for(int i=1; i<=m;i++){
+        for(int j=1; j<=n;j++){
             graph[i][j] = 0;
         }
     }
+    // 1. read the edges and fill the graph matrix
+    // if there is an edge between up and down, then graph[up][down] = 1 and graph[down][up] = 1
     int up,down;
-    for (int i=0; i<m;i++){
+    for (int i=0; i<=m;i++){
         fscanf(f, "%d%d", &up, &down);
+        if(up<=n && down<=n){
         graph[up][down] = 1;
         graph[down][up] = 1;
+        }
     }
     
-    for(int i=0; i<n;i++){
-        for(int j=0; j<n;j++){
+    for(int i=1; i<=n;i++){
+        for(int j=1; j<=n;j++){
             printf("%d ", graph[i][j]);
         }
         printf("\n");

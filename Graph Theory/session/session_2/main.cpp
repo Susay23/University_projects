33ab1f3f861698c6962f9ff045cf7b5868e7dd4f
Susay23 <buituananh29112006@gmail.com>
2026-06-09 03:27:24 +0000

    Add initial implementation of graph data structure and CMake build files
    - Implemented a simple graph structure with functions to initialize the graph and add edges.
    - Created CMake configuration files for building the project, including Makefile and dependency management.
    - Added source file `main.cpp` to demonstrate graph functionality.
    - Generated necessary build artifacts and configuration files for CMake.

--- Graph Theory/session/session_2/main.cpp
+++ Graph Theory/session/session_2/main.cpp
@@ -1,1 +1,44 @@
-#include <>
+#include <stdio.h>
+
+#define MAX_EDGES 1000
+
+typedef struct  {
+	 int x, y;
+} Edge;
+typedef struct {
+	int n, m;
+	Edge edges[MAX_EDGES];
+} Graph;
+
+void init_graph(Graph* G, int n){
+	G->n = n;
+	G->m = 0;
+}
+
+void add_edge(Graph* G, int x, int y){
+    if(G->m < MAX_EDGES){
+        if(x <= G->n && y <= G->n){
+            G->edges[G->m].x = x;
+            G->edges[G->m].y = y;
+            G->m++;
+        }
+    }
+}
+
+int main(){
+    Graph G;
+    init_graph(&G, 5);
+    add_edge(&G, 1, 2);
+    add_edge(&G, 1, 3);
+    add_edge(&G, 2, 4);
+    add_edge(&G, 3, 4);
+    add_edge(&G, 4, 5);
+
+    printf("Number of vertices: %d\n", G.n);
+    printf("Number of edges: %d\n", G.m);
+    for(int i=0; i<G.m; i++){
+        printf("Edge %d: (%d, %d)\n", i+1, G.edges[i].x, G.edges[i].y);
+    }
+
+    return 0;
+}

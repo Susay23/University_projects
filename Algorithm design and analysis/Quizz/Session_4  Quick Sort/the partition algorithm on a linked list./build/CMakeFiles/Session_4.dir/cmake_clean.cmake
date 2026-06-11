530ef7612cf364cd2f3ad2dc6737ea1b3d985e87
Susay23 <buituananh29112006@gmail.com>
2026-06-11 01:25:35 +0000

    Add CMake build system and initial implementation for partition algorithm on linked list
    - Created CMake configuration files for building the project.
    - Added main.cpp with basic includes for implementation.
    - Implemented run.sh script for building and executing the project.
    - Included necessary build files and directories for CMake.
    - Established a clean structure for managing build dependencies and targets.

---
+++ Algorithm design and analysis/Quizz/Session_4  Quick Sort/the partition algorithm on a linked list./build/CMakeFiles/Session_4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Session_4.dir/main.cpp.o"
+  "CMakeFiles/Session_4.dir/main.cpp.o.d"
+  "Session_4"
+  "Session_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Session_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Algorithm design and analysis/Quizz/Session_4  Quick Sort/the partition algorithm on a linked list./build/CMakeFiles/Session_4.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/Session_4.dir/main.cpp.o: \
+ /workspaces/University_projects/Algorithm\ design\ and\ analysis/Quizz/Session_4\ \ Quick\ Sort/the\ partition\ algorithm\ on\ a\ linked\ list./main.cpp \
+ /usr/include/stdc-predef.h

3f1a0b3c116a0a0dfd35a56e0c77b53ca7d62afe
Susay23 <buituananh29112006@gmail.com>
2026-06-24 02:36:03 +0000

    Add initial implementation of graph traversal using BFS
    - Created a new C++ project structure with CMake build system.
    - Implemented a Graph data structure and functions to initialize the graph and add edges.
    - Developed a Queue data structure for BFS traversal.
    - Implemented the BFS algorithm to traverse the graph and print visited nodes.
    - Added input handling to read graph data from a file (dt.txt).
    - Included a shell script (run.sh) to automate the build and execution process.
    - Generated necessary CMake files for building the project.

---
+++ Graph Theory/pratice_2/session 1/build/CMakeFiles/session_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session_1.dir/main.cpp.o"
+  "CMakeFiles/session_1.dir/main.cpp.o.d"
+  "session_1"
+  "session_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

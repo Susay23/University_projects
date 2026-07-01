ca54dc33750ef8a936dbab913d76365b18622584
Susay23 <buituananh29112006@gmail.com>
2026-07-01 09:38:22 +0700

    Add SQL table definitions and sample data for TACPHAM, DOCGIA, SACH, and MUON

---
+++ DataBase/session/session_2.sql
@@ -0,0 +1,215 @@
+-- TACPHAM(NT, tua, tacgia)
+-- DOCGIA (ND, ho, ten, dchi, tel)
+-- SACH (NS, nxb, NT)
+-- MUON (NS, ngaymuon, hantra, ngaytra, ND) ;
+
+create table TACPHAM(
+    NT smallint primary key,
+    tua nvarchar(50),
+    tacgia nvarchar(50)
+);
+
+create table DOCGIA(
+    ND smallint primary key,
+    ho nvarchar(50),
+    ten nvarchar(50),
+    dchi nvarchar(100),
+    tel char(50)
+);
+
+create table SACH(
+    NS smallint primary key,
+    nxb nvarchar(50),
+    NT smallint not null,
+    foreign key(NT) references TACPHAM(NT)
+);
+
+create table MUON(
+    NS smallint not null,
+    foreign key(NS) references SACH(NS),
+    ngaymuon date not null,
+    hantra date not null,
+    ngaytra date,
+    ND smallint not null,
+    foreign key(ND) references DOCGIA(ND),
+);
+
+-- TACPHAM
+INSERT INTO TACPHAM VALUES (1, N'Les misérables', N'Victor Hugo');
+INSERT INTO TACPHAM VALUES (2, N'Germinal', N'Émile Zola');
+INSERT INTO TACPHAM VALUES (3, N'Madame Bovary', N'Gustave Flaubert');
+INSERT INTO TACPHAM VALUES (4, N'Les Fleurs du mal', N'Charles Baudelaire');
+INSERT INTO TACPHAM VALUES (5, N'La Bête Humaine', N'Émile Zola');
+INSERT INTO TACPHAM VALUES (6, N'Le Père Goriot', N'Honoré de Balzac');
+INSERT INTO TACPHAM VALUES (7, N'Le Rouge et le Noir', N'Stendhal');
+INSERT INTO TACPHAM VALUES (8, N'Une vie', N'Guy de Maupassant');
+INSERT INTO TACPHAM VALUES (9, N'Ubu Roi', N'Alfred Jarry');
+INSERT INTO TACPHAM VALUES (10, N'Poésies', N'Arthur Rimbaud');
+INSERT INTO TACPHAM VALUES (11, N'La Mare au Diable', N'George Sand');
+INSERT INTO TACPHAM VALUES (12, N'Carmen', N'Prosper Mérimée');
+INSERT INTO TACPHAM VALUES (13, N'Lettres de mon moulin', N'Alphonse Daudet');
+INSERT INTO TACPHAM VALUES (14, N'Les contes du chat perché', N'Marcel Aymé');
+INSERT INTO TACPHAM VALUES (15, N'La Peste', N'Albert Camus');
+INSERT INTO TACPHAM VALUES (16, N'Le petit prince', N'Antoine de St Exupéry');
+-- DOCGIA
+INSERT INTO DOCGIA VALUES (1, N'Lecoeur', N'Jeanette', N'16 rue de la République, 75010 Paris', '0145279274');
+INSERT INTO DOCGIA VALUES (2, N'Lecoeur', N'Philippe', N'16 rue de la République, 75010 Paris', '0145279274');
+INSERT INTO DOCGIA VALUES (3, N'Dupont', N'Yvan', N'73 rue Lamarck, 75018 Paris', '0163538294');
+INSERT INTO DOCGIA VALUES (4, N'Mercier', N'Claude', N'155 avenue Parmentier, 75011 Paris', '0136482736');
+INSERT INTO DOCGIA VALUES (5, N'Léger', N'Marc', N'90 avenue du Maine, 75014 Paris', '0164832947');
+INSERT INTO DOCGIA VALUES (6, N'Martin', N'Laure', N'51 boulevard Diderot, 75012 Paris', '0174693277');
+INSERT INTO DOCGIA VALUES (7, N'Crozier', N'Martine', N'88 rue des Portes Blanches, 75018 Paris', '0146829384');
+INSERT INTO DOCGIA VALUES (8, N'Lebon', N'Clément', N'196 boulevard de Sébastopol, 75001 Paris', '0132884739');
+INSERT INTO DOCGIA VALUES (9, N'Dufour', N'Jacques', N'32 rue des Alouettes, 75003 Paris', '0155382940');
+INSERT INTO DOCGIA VALUES (10, N'Dufour', N'Antoine', N'32 rue des Alouettes, 75003 Paris', '0155382940');
+INSERT INTO DOCGIA VALUES (11, N'Dufour', N'Stéphanie', N'32 rue des Alouettes, 75003 Paris', '0155382940');
+INSERT INTO DOCGIA VALUES (12, N'Raymond', N'Carole', N'35 rue Oberkampf, 75011 Paris', '0153829402');
+INSERT INTO DOCGIA VALUES (13, N'Durand', N'Albert', N'4 rue Mandar, 75002 Paris', '0642374021');
+INSERT INTO DOCGIA VALUES (14, N'Wilson', N'Paul', N'12 rue Paul Vaillant Couturier, 92400 Montrouge', '0642327407');
+INSERT INTO DOCGIA VALUES (15, N'Grecault', N'Philippe', N'15 rue de la Roquette, 75012 Paris', '0132762983');
+INSERT INTO DOCGIA VALUES (16, N'Carre', N'Stéphane', N'51 boulevard Diderot, 75012 Paris', '0174693277');
+INSERT INTO DOCGIA VALUES (17, N'Johnson', N'Astrid', N'3 rue Léon Blum, 75002 Paris', '0143762947');
+INSERT INTO DOCGIA VALUES (18, N'Mirou', N'Caroline', N'2 square Mirabeau, 75011 Paris', '0163827399');
+INSERT INTO DOCGIA VALUES (19, N'Espelette', N'Jean-Jacques', N'141 avenue de France, 75019 Paris', '0134887264');
+INSERT INTO DOCGIA VALUES (20, N'Despentes', N'Anthony', N'56 boulevard de la Villette, 75019 Paris', '0133889463');
+INSERT INTO DOCGIA VALUES (21, N'Terlu', N'Véronique', N'45 rue des Batignolles, 75020 Paris', '0165998372');
+INSERT INTO DOCGIA VALUES (22, N'Rihour', N'Cécile', N'7 rue Montorgueil, 75002 Paris', '0166894754');
+INSERT INTO DOCGIA VALUES (23, N'Franchet', N'Pierre', N'160 rue Montmartre, 75009 Paris', '0633628549');
+INSERT INTO DOCGIA VALUES (24, N'Trochet', N'Ernest', N'34 rue de l''Esperance, 75008 Paris', '0638295563');
+INSERT INTO DOCGIA VALUES (25, N'Gainard', N'Simon', N'55 rue Desnouettes, 75015 Paris', '0174928934');
+INSERT INTO DOCGIA VALUES (26, N'Touche', N'Johanna', N'14 rue du Bac, 75006 Paris', '0619384065');
+INSERT INTO DOCGIA VALUES (27, N'Cornu', N'Sylvain', N'22 rue Mouffetard, 75005 Paris', '0184927489');
+INSERT INTO DOCGIA VALUES (28, N'Frederic', N'Cyril', N'15 rue du Simplon, 75018 Paris', '0173625492');
+INSERT INTO DOCGIA VALUES (29, N'Crestard', N'Cedric', N'5 rue Doudeauville, 75018 Paris', '0629485700');
+INSERT INTO DOCGIA VALUES (30, N'Le Bihan', N'Karine', N'170 bis rue Ordener, 75018 Paris', '0638995221');
+-- SACH
+INSERT INTO SACH VALUES (1, N'GF', 1);
+INSERT INTO SACH VALUES (2, N'FOLIO', 2);
+INSERT INTO SACH VALUES (3, N'HACHETTE', 3);
+INSERT INTO SACH VALUES (4, N'GF', 4);
+INSERT INTO SACH VALUES (5, N'FOLIO', 5);
+INSERT INTO SACH VALUES (6, N'FOLIO', 6);
+INSERT INTO SACH VALUES (7, N'GF', 7);
+INSERT INTO SACH VALUES (8, N'FOLIO', 8);
+INSERT INTO SACH VALUES (9, N'HACHETTE', 9);
+INSERT INTO SACH VALUES (10, N'GF', 10);
+INSERT INTO SACH VALUES (11, N'HACHETTE', 11);
+INSERT INTO SACH VALUES (12, N'FOLIO', 12);
+INSERT INTO SACH VALUES (13, N'GF', 13);
+INSERT INTO SACH VALUES (14, N'FOLIO', 14);
+INSERT INTO SACH VALUES (15, N'HACHETTE', 15);
+INSERT INTO SACH VALUES (16, N'HACHETTE', 16);
+INSERT INTO SACH VALUES (17, N'GF', 1);
+INSERT INTO SACH VALUES (18, N'FOLIO', 2);
+INSERT INTO SACH VALUES (19, N'HACHETTE', 2);
+INSERT INTO SACH VALUES (20, N'FOLIO', 4);
+INSERT INTO SACH VALUES (21, N'GF', 5);
+INSERT INTO SACH VALUES (22, N'HACHETTE', 4);
+INSERT INTO SACH VALUES (23, N'HACHETTE', 7);
+INSERT INTO SACH VALUES (24, N'FOLIO', 8);
+INSERT INTO SACH VALUES (25, N'GF', 1);
+INSERT INTO SACH VALUES (26, N'HACHETTE', 10);
+INSERT INTO SACH VALUES (27, N'FOLIO', 11);
+INSERT INTO SACH VALUES (28, N'FOLIO', 12);
+INSERT INTO SACH VALUES (29, N'GF', 1);
+INSERT INTO SACH VALUES (30, N'HACHETTE', 14);
+-- MUON
+
+INSERT INTO MUON VALUES (1,'9-2-2007','9-16-2007','9-7-2007',1);
+INSERT INTO MUON VALUES (1,'10-1-2006','10-15-2006','10-11-2006',26);
+INSERT INTO MUON VALUES (1,'6-14-2007','6-28-2007','6-19-2007',2);
+
+INSERT INTO MUON VALUES (1,'4-27-2007','5-11-2007','5-8-2007',3);
+INSERT INTO MUON VALUES (2,'8-23-2007','9-6-2007','9-2-2007',4);
+INSERT INTO MUON VALUES (2,'10-6-2007','10-20-2007', NULL,28);
+
+INSERT INTO MUON VALUES (9,'10-6-2007','10-20-2007', NULL,28);
+INSERT INTO MUON VALUES (3,'9-9-2007','9-23-2007','9-13-2007',3);
+INSERT INTO MUON VALUES (4,'2-8-2007','2-22-2007','2-12-2007',12);
+INSERT INTO MUON VALUES (4,'2-7-2006','2-21-2006','2-20-2006',4);
+
+INSERT INTO MUON VALUES (4,'6-17-2007','7-1-2007','6-27-2007',5);
+INSERT INTO MUON VALUES (5,'10-4-2007','10-19-2007', NULL,16);
+INSERT INTO MUON VALUES (6,'3-11-2007','3-25-2007','3-16-2007',3);
+
+INSERT INTO MUON VALUES (8,'7-14-2007','7-28-2007','7-20-2007',18);
+INSERT INTO MUON VALUES (8,'3-9-2007','3-23-2007','3-27-2007',13);
+
+INSERT INTO MUON VALUES (10,'4-11-2007','4-25-2007','4-23-2007',8);
+INSERT INTO MUON VALUES (10,'1-27-2006','2-10-2006','1-31-2006',7);
+INSERT INTO MUON VALUES (11,'10-1-2007','10-15-2007', NULL,22);
+
+INSERT INTO MUON VALUES (12,'3-3-2003','3-17-2003','3-13-2003',7);
+INSERT INTO MUON VALUES (14,'10-2-2007','10-16-2007', NULL,1);
+INSERT INTO MUON VALUES (15,'5-4-2007','5-18-2007','5-12-2007',10);
+
+INSERT INTO MUON VALUES (16,'10-5-2007','10-19-2007', NULL,2);
+INSERT INTO MUON VALUES (18,'7-4-2007','7-18-2007','7-11-2007',3);
+INSERT INTO MUON VALUES (20,'12-17-2006','12-31-2006','12-17-2006',27);
+
+INSERT INTO MUON VALUES (20,'9-6-2007','9-20-2007','9-22-2007',9);
+INSERT INTO MUON VALUES (21,'9-7-2007','9-21-2007','9-19-2007',14);
+INSERT INTO MUON VALUES (24,'2-14-2007','2-28-2007','2-20-2007',26);
+INSERT INTO MUON VALUES (25,'8-19-2006','9-2-2006','9-3-2006',1);
+INSERT INTO MUON VALUES (26,'11-15-2006','11-29-2006','11-22-2006',9);
+INSERT INTO MUON VALUES (29,'9-20-2007','10-4-2007', NULL,27);
+
+-- 1) Xem dữ liệu của tất cả các bảng
+select * from TACPHAM;
+select * from DOCGIA;
+select * from SACH;
+select * from MUON;
+-- 2) Vẽ sơ đồ liên thông
+-- +-------------------+              +-------------------+
+-- |     TACPHAM       |              |      DOCGIA       |
+-- +-------------------+              +-------------------+
+-- | PK | MaTacPham    |              | PK | MaDocGia     |
+-- |    | TenTacPham   |              |    | Ho           |
+-- |    | TacGia       |              |    | Ten          |
+-- +-------------------+              |    | DChi         |
+--           |                        |    | DThoai       |
+--           | 1                      +-------------------+
+--           |                                  |
+--           | N                                | 1
+-- +-------------------+                        |
+-- |       SACH        |                        |
+-- +-------------------+                        |
+-- | PK | MaSach       |                        |
+-- |    | NhaXuatBan   |                        | N
+-- | FK | MaTacPham    | ---------------> [ BẢNG TRUNG GIAN ]
+-- +-------------------+                    (Nếu có: MUON_TRA)
+--                                          - MaDocGia (FK)
+--                                          - MaSach (FK)
+--                                          - NgayMuon
+-- 3) Các tác phẩm (NT, tựa) của tác giả 'Guy de Maupassant'.
+select NT, tua from TACPHAM where tacgia = 'Guy de Maupassant';
+-- 4) Các độc giả sống ở địa chỉ '32 rue des Alouettes, 75003 Paris'.
+select * from DOCGIA where dchi = '32 rue des Alouettes, 75003 Paris';
+-- 5) Tìm tên nhà xuất bản các tác phẩm bao gồm từ 'Fleur'
+select nxb from SACH WHERE NT in (select nt from TACPHAM where tua like '%Fleur%');
+-- 6) Tìm tên các tác phẩm bắt đầu bằng 'Le'
+select tua from TACPHAM where tua like 'Le%';
+-- 7) Tìm tên các độc giả có mượn sách trong trong khoảng thời gian từ ngày 15/9/2007 đến
+-- 20/09/2007
+select * from DOCGIA where EXISTS(SELECT * from MUON where ngaymuon BETWEEN '2007-09-15' AND '2007-09-20');
+-- 8) Nhà xuất bản của tác phẩm tiêu đề 'Germinal'.
+-- 9) Tên độc giả đã mượn tác phẩm 'Poésie'.
+-- 10) Những độc giả nào đã mượn tác phẩm Les 'Fleurs du mal'
+-- 11) Tìm các tựa sách, tên độc giả của các độc giả trả sách quá thời hạn cho phép
+-- 12) Tìm các tựa sách, tên độc giả của các độc giả trả sách trước thời hạn
+-- 13) Tên độc giả đã mượn tác phẩm của 'Victor Hugo'
+-- 14) Tên độc giả và các tác phẩm đã được mượn năm 2007
+-- 15) Tính số tác phẩm có trong thư viện.
+-- —------------------------------------------------------------------------------------------
+-- 16) Tính Số sách của mỗi tác phẩm
+-- 17) Tìm tên của tác phẩm mà có ít nhất hai quyển sách.
+-- 18) Tìm tên tác phẩm có ít nhất 3 sách
+-- 19) Tính Số tác phẩm của mỗi tác giả.
+-- 20) Tính Số tác giả có ít nhất hai tác phẩm
+-- 21) Tìm số lần mượn sách của mỗi độc giả theo năm
+-- 22) Tìm tên tác phẩm có nhiều sách nhất
+-- 23) Tên nhà xuất bản xuất bản nhiều sách nhất
+-- 24) Tìm tên tác phẩm có ít nhất một quyển sách không ai mượn
+-- 25) Tìm tên tác phẩm có tất cả các quyển sách đều được mượn
+-- 26) Tìm họ tên độc giả chưa mượn quyển sách nào
+-- 27) Tìm thông tin về nhà xuất bản của quyển sách mà chưa được ai mượn

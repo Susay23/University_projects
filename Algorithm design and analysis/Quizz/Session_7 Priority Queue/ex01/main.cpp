eea55c1c3cd458e0dfc95fb7376930273b802630
Susay23 <buituananh29112006@gmail.com>
2026-07-02 03:29:53 +0000

    Add pseudocode comments for the solve function and document complexity analysis for build_maxheap

--- Algorithm design and analysis/Quizz/Session_7 Priority Queue/ex01/main.cpp
+++ Algorithm design and analysis/Quizz/Session_7 Priority Queue/ex01/main.cpp
@@ -2,6 +2,24 @@
 #include<iostream>
 using namespace std;
 
+// function solve():
+//     read n
+//     read array A[1..n]
+
+//     current_max = -infinity
+//     for i = 1 to n:
+//         current_max = max(current_max, A[i])
+
+//     read q
+//     for i = 1 to q:
+//         read type
+
+//         if type == 1:
+//             read x
+//             current_max = max(current_max, x)
+//         else:  // type == 2
+//             print current_max
+
 void solve(){
     int n;
     cin>>n;

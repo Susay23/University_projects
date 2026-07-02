00cd8be57dd0fa9b70bc49455d0a3c53118bcbc7
Susay23 <buituananh29112006@gmail.com>
2026-07-02 03:10:35 +0000

    Implement main functionality for Session 7 Priority Queue and update build files

--- Algorithm design and analysis/Quizz/Session_7 Priority Queue/build/CMakeFiles/Session_7.dir/link.d
+++ Algorithm design and analysis/Quizz/Session_7 Priority Queue/build/CMakeFiles/Session_7.dir/link.d
@@ -29,7 +29,9 @@ Session_7: \
   /usr/lib/gcc/x86_64-linux-gnu/15/libgcc.a \
   /usr/lib/gcc/x86_64-linux-gnu/15/crtendS.o \
   /usr/lib/gcc/x86_64-linux-gnu/15/../../../x86_64-linux-gnu/crtn.o \
-  /lib64/ld-linux-x86-64.so.2
+  /usr/lib/x86_64-linux-gnu/libm.so.6 \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/15/../../../x86_64-linux-gnu/libgcc_s.so.1
 
 /usr/lib/gcc/x86_64-linux-gnu/15/../../../x86_64-linux-gnu/Scrt1.o:
 
@@ -91,4 +93,8 @@ CMakeFiles/Session_7.dir/main.cpp.o:
 
 /usr/lib/gcc/x86_64-linux-gnu/15/../../../x86_64-linux-gnu/crtn.o:
 
+/usr/lib/x86_64-linux-gnu/libm.so.6:
+
 /lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/15/../../../x86_64-linux-gnu/libgcc_s.so.1:
---
+++ Algorithm design and analysis/Quizz/Session_7 Priority Queue/main.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    int a[n+1];
+    for(int i = 1;i<=n;i++){
+        cin>>a[i];
+    }
+    int current_max = a[1];
+    for(int i = 1;i<=n;i++){
+        if(a[i]>current_max){
+            current_max = a[i];
+        }
+    }
+
+    int q;
+    cin>>q;
+    for(int i = 1; i<=q;i++){
+        int type;
+        cin>>type;
+        if(type == 1){
+            int x;
+            cin>>x;
+            if(x>current_max){
+                current_max = x;
+            }
+        }
+        else{
+                cout<<current_max;
+                cout<<endl;
+        }
+    }
+}
+
+int main(){
+    freopen("ex01_01.inp","r",stdin);
+    solve();
+    return 0;
+}

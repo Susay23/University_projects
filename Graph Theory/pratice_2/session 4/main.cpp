6b8ed9d91c28ae4a6f6040b476a7300a8cc0d541
Susay23 <buituananh29112006@gmail.com>
2026-07-10 08:46:09 +0000

    feat: implement graph initialization and edge addition functions

--- Graph Theory/pratice_2/session 4/main.cpp
+++ Graph Theory/pratice_2/session 4/main.cpp
@@ -5,3 +5,30 @@ typedef struct{
     int n;
 }Graph;
 
+void init_graph(Graph *g, int n){
+    g->n = n;
+    for(int i = 0; i<=n;i++){
+        for(int j = 0; j<=n;j++){
+            g->A[i][j]=0;
+        }
+    }
+}
+
+void add_edge(Graph *g, int u, int v){
+    g->A[u][v] = 1;
+    g->A[v][u] = 1;
+}
+
+
+int main(){
+    freopen("dt.txt","r",stdin);
+    int n,m;
+    scanf("%d%d ", &n, &m);
+    Graph G;
+    init_graph(&G,n);
+    int u,v;
+    for(int i = 0; i<m;i++){
+        scanf("%d%d ", &u,&v);
+        add_edge(&G, u, v);
+    }
+}

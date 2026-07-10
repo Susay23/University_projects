15aa7f37f65fa2a228aa31c334fdc74313740014
Susay23 <buituananh29112006@gmail.com>
2026-07-10 10:35:20 +0000

    feat: implement BFS algorithm and queue operations for graph traversal

--- Graph Theory/pratice_2/session 4/main.cpp
+++ Graph Theory/pratice_2/session 4/main.cpp
@@ -5,6 +5,12 @@ typedef struct{
     int n;
 }Graph;
 
+typedef struct{
+    int data[1000];
+    int front;
+    int rear;
+}Queue;
+
 void init_graph(Graph *g, int n){
     g->n = n;
     for(int i = 0; i<=n;i++){
@@ -20,15 +26,72 @@ void add_edge(Graph *g, int u, int v){
 }
 
 
+void initQueue(Queue *Q){
+    Q->front = 0;
+    Q->rear = 0;
+}
+
+int isEmpty(Queue *Q){
+    if(Q->front == Q->rear){
+        return 1;
+    }
+    return 0;
+}
+
+void enqueue(Queue *Q, int x){
+    Q->data[Q->rear] = x;
+    Q->rear = Q->rear + 1;
+}
+
+int dequeue(Queue *Q){
+    int x = Q->data[Q->front];
+    Q->front = Q->front + 1;
+    return x;
+}
+
+void BFS(Graph *G, int start, int visited[], int parent[]){
+    Queue q;
+    initQueue(&q);
+    visited[start] = 1;
+    parent[start] = 0;
+    enqueue(&q,start);
+
+    while(!isEmpty(&q)){
+        int u = dequeue(&q);
+        if(parent[u] == 0){
+            printf("%d có cha là đỉnh %d => %d là đỉnh gốc \n", u, parent[u], u);
+        }else{
+            printf("%d có cha là đỉnh %d \n", u, parent[u]);
+        }
+
+        for(int i = 1; i <= G->n; i++){
+            if(G->A[u][i] == 1 && visited[i] == 0){
+                visited[i] = 1;
+                parent[i] = u;
+                enqueue(&q,i);
+            }
+        }
+    }
+}
+
+
 int main(){
     freopen("dt.txt","r",stdin);
     int n,m;
     scanf("%d%d ", &n, &m);
     Graph G;
+    G.n = n;
     init_graph(&G,n);
     int u,v;
     for(int i = 0; i<m;i++){
         scanf("%d%d ", &u,&v);
         add_edge(&G, u, v);
+    }
+    int visited[101] = {0};
+    int parent[105];
+    for(int i = 1; i <= n; i ++){
+        if(visited[i] == 0){
+            BFS(&G,i,visited, parent);
+        }
     }
 }

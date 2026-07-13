6d9639382bf50888116eed167390a90ea06c2178
Susay23 <buituananh29112006@gmail.com>
2026-07-13 03:44:15 +0000

    feat: add CMake configuration and implement graph traversal with DFS

--- Graph Theory/pratice_2/session 6/main.cpp
+++ Graph Theory/pratice_2/session 6/main.cpp
@@ -1,80 +1,55 @@
-// Cho một đồ thị vô hướng đơn. Hãy dựng (các) cây DUYỆT ĐỒ THỊ khi duyệt đồ thị theo chiều sâu (dùng ĐỆ QUY) bắt đầu từ đỉnh 1.
+#include <stdio.h>
 
-// Nếu đồ thị không liên thông, sau khi duyệt xong lần 1, tìm đỉnh có chỉ số nhỏ nhất chưa duyệt mà duyệt nó, và cứ tiếp tục như thế cho đến khi tất cả các đỉnh đều được duyệt.
+typedef struct{
+    int A[101][101];
+    int n;
+}Graph;
 
-// Quy ước:
+void init_graph(Graph *G, int n){
+    G->n = n;
+    for(int i = 0; i<=n;i++){
+        for(int j = 0; j <= n; j++){
+            G->A[i][j] = 0;
+        }
+    }
+}
 
-// Các đỉnh kề của 1 đỉnh được liệt kê theo thứ tự tăng dần.
+void add_edge(Graph *G, int u, int v){
+    G->A[u][v] = 1;
+    G->A[v][u] = 1;
+}
 
+void DFS(Graph *G, int start, int visited[], int parent){
+    visited[start] = 1;
+    if(parent == 0){
+        printf("%d có đỉnh cha là %d => %d là gốc \n", start, parent, start);
+    }else{
+        printf("%d có đỉnh cha là %d\n", start, parent);
+    }   
+    for(int i = 1; i<=G->n;i++){
+        if(G->A[start][i] == 1 && visited[i] == 0){
+            DFS(G,i,visited,start);
+        }
+    }
+}
 
-// Đầu vào (Input):
-// Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+int main(){
+    freopen("dt.txt", "r", stdin);
+    int n,m;
+    scanf("%d%d", &n,&m);
+    Graph G;
+    init_graph(&G,n);
+    int u,v;
+    for(int i = 0; i < m;i++){
+        scanf("%d%d", &u,&v);
+        add_edge(&G,u,v);
+    }
 
-// - Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số đỉnh và số cung.
-
-// - m dòng tiếp theo mỗi dòng chứa 2 số nguyên u v nói rằng mô tả cung (u, v).
-
-// Đầu ra (Output):
-
-// In ra cây duyệt đồ thị theo định dạng:
-
-// 1 <đỉnh cha của 1>
-// 2 <đỉnh cha của 2>
-// ....
-// i <đỉnh cha của i>
-// ...
-// n <đỉnh cha của n>
-
-// Quy ước: Nếu 1 đỉnh không có đỉnh cha (nó là đỉnh gốc của cây) thì đỉnh cha của nó là 0.
-
-// Xem thêm các ví dụ bên dưới.
-
-
-
-// Gợi ý:
-
-// Sử dụng mảng parent[u] để lưu đỉnh cha của đỉnh u.
-// Trong quá trình duyệt, thay vì in các đỉnh ra màn hình, ghi nhận lại đỉnh cha của các đỉnh.
-// Thêm 1 tham số p (đỉnh cha của đỉnh u) cho hàm visit(). Khi gọi đệ quy duyệt v ta truyền u như là đỉnh cha của v.
-// void visit(Graph* G, int u, int p) {
-//     ...
-//     for (các đỉnh kề v của u)
-//         visit(G, v, u);
-// }
-// Khi duyệt xong lần lượt in ra u và parent[u] (u chạy từ 1 đến n).
-
-
-// Hướng dẫn đọc dữ liệu và chạy thử chương trình
-// Để chạy thử chương trình, bạn nên tạo một tập tin dt.txt chứa đồ thị cần kiểm tra.
-// Thêm dòng freopen("dt.txt", "r", stdin); vào ngay sau hàm main(). Khi nộp bài, nhớ gỡ bỏ dòng này ra.
-// Có thể sử dụng đoạn chương trình đọc dữ liệu mẫu sau đây:
-// 	freopen("dt.txt", "r", stdin); //Khi nộp bài nhớ bỏ dòng này.
-// 	Graph G;
-// 	int n, m, u, v, w, e;
-// 	scanf("%d%d", &n, &m);
-// 	init_graph(&G, n);
-	
-// 	for (e = 0; e < m; e++) {
-// 		scanf("%d%d", &u, &v);
-// 		add_edge(&G, u, v);
-// 	}
-
-
-// For example:
-// 13 16
-// 1 4
-// 1 2
-// 1 12
-// 2 4
-// 3 7
-// 4 6
-// 4 7
-// 5 6
-// 5 8
-// 5 9
-// 6 7
-// 6 13
-// 8 9
-// 10 11
-// 10 12
-// 11 12
+    int visited[101];
+    int parent = 0;
+    for(int i = 1; i<=n;i++){
+        if(visited[i] == 0){
+            DFS(&G,i,visited,parent);
+        }
+    }
+}
